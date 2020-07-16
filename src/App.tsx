6cd7ff3d80f25907d0a5b4e9589cbfceb8a4c046
RubenZx <ruben.mondom@gmail.com>
2020-07-16 12:38:10 +0200

    :zap: added the previous change to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import {
+  CssBaseline,
+  MuiThemeProvider,
+  NoSsr,
+  StylesProvider,
+} from '@material-ui/core'
 import React from 'react'
-import Table from './components/table'
+import { ThemeProvider } from 'styled-components'
+import Layout from './components/layout'
+import theme from './theme'
 
 const App = () => {
-  return <Table />
+  return (
+    <>
+      <CssBaseline />
+      <NoSsr>
+        <StylesProvider injectFirst>
+          {/* To override the current theme */}
+          <MuiThemeProvider theme={theme}>
+            {/* Allow share the theme with styled components */}
+            <ThemeProvider theme={theme}>
+              <Layout />
+            </ThemeProvider>
+          </MuiThemeProvider>
+        </StylesProvider>
+      </NoSsr>
+    </>
+  )
 }
 
 export default App
