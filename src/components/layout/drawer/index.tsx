1c9b443f14fe1c195fb0d969d4ccee7efc50dfd4
RubenZx <ruben.mondom@gmail.com>
2020-07-17 13:20:06 +0200

    :zap: new layout with permanent left side menu

---
+++ src/components/layout/drawer/index.tsx
@@ -0,0 +1,78 @@
+import {
+  createStyles,
+  Drawer,
+  Grid,
+  List,
+  makeStyles,
+  Theme,
+} from '@material-ui/core'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday'
+import SettingsIcon from '@material-ui/icons/Settings'
+import React from 'react'
+import { useHistory } from 'react-router'
+import Logo from '../../../assets/logo.gif'
+import { styled } from '../../../theme'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawerPaper: {
+      width: 240,
+    },
+  }),
+)
+
+const StyledDrawer = styled(Drawer)`
+  width: 240px;
+  flex-shrink: 0;
+`
+
+const MenuElements = [
+  { text: 'Horario', icon: <CalendarTodayIcon />, route: '/' },
+  { text: 'Ajustes', icon: <SettingsIcon />, route: '/settings' },
+]
+
+const MyDrawer = () => {
+  const classes = useStyles()
+  const history = useHistory()
+
+  return (
+    <StyledDrawer
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      anchor="left"
+    >
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        style={{ minHeight: '80px', marginTop: '10px' }}
+      >
+        <img width={180} src={Logo} alt="logoUCA" />
+      </Grid>
+      <Divider />
+      <List>
+        {MenuElements.map(({ text, icon, route }) => (
+          <ListItem
+            button
+            key={text}
+            onClick={() => {
+              history.push(route)
+            }}
+          >
+            <ListItemIcon>{icon}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </StyledDrawer>
+  )
+}
+
+export default MyDrawer
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,13 +1,28 @@
+import { Grid } from '@material-ui/core'
 import React from 'react'
-import Table from '../table'
-import NavBar from './NavBar'
+import { styled } from '../../theme'
+import MyDrawer from './drawer'
 
-const Layout = () => {
+const Container = styled(Grid)`
+  flex-grow: 1;
+  margin-top: 90px;
+  padding: 10px 30px;
+`
+
+const Root = styled.div`
+  display: flex;
+`
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
-      <NavBar />
-      <Table />
-    </>
+    <Root>
+      <MyDrawer />
+      <Container container>{children}</Container>
+    </Root>
   )
 }
 
