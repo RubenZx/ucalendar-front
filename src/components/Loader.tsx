0f1b31248787db5f07a88fe661e6e9d4f2598e66
RubenZx <ruben.mondom@gmail.com>
2020-09-04 11:56:44 +0200

    :sparkles: new flexbox component with spinner for loading

---
+++ src/components/Loader.tsx
@@ -0,0 +1,10 @@
+import { Box, BoxProps, CircularProgress, Typography } from '@material-ui/core'
+import React from 'react'
+
+export default (props: BoxProps) => (
+  <Box display="flex" {...props}>
+    <CircularProgress />
+    <Box m={1} />
+    <Typography>Cargando...</Typography>
+  </Box>
+)
